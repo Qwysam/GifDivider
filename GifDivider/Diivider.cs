a327f4021fd8de2d6064c9cbee178afc08148027
qwysam <zhdanoov777@gmail.com>
2023-10-02 17:20:15 +0200

    Initial build

---
+++ GifDivider/Diivider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace GifDivider
+{
+
+	public static class Divider
+	{
+        public static Image[] GifToFrames(Image img)
+        {
+            Image[] arr = new Image[img.Frames.Count];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = img.Frames.CloneFrame(i);
+            return arr;
+        }
+
+        public static void SaveImages(Image[] arr, string outputDirectory)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i].SaveAsJpeg(outputDirectory + $"/{i}.jpeg");
+            }
+        }
+    }
+}
+
---
+++ GifDivider/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using GifDivider;
+string inputGifPath = "";
+string outputDirectory = "";
+Image tmp;
+if(args.Length == 0)
+{
+    Console.WriteLine("Incorrect file path");
+}
+else
+{
+    inputGifPath = args[0];
+    tmp = Image.Load(inputGifPath);
+    if (args.Length == 1)
+    {
+        outputDirectory = Directory.GetParent(inputGifPath) + "/Images";
+        Directory.CreateDirectory(outputDirectory);
+    }
+    else
+        outputDirectory = args[1];
+    if (Directory.Exists(outputDirectory))
+        Divider.SaveImages(Divider.GifToFrames(tmp), outputDirectory);
+    else
+        Console.WriteLine("Incorrect output directory");
+}
